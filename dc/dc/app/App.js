fb6bfe07813fc5a7d17410516f95919f3e6ce2cc
Juan Manuel Moreno <juan.moreno@diproach.com>
2014-01-02 16:32:32 -0300

    Cambio de lenguaje en la interfaz

--- dc/dc/app/App.js
+++ dc/dc/app/App.js
@@ -4,10 +4,39 @@ window.App = {
 
 	isEnvironmentWeb:function(){
 		return true;
-	}
-	,
-	adScreenToShow:0
-	,
+	},
+
+	adScreenToShow:0,
+	
+	language:"",
+	
+	translate:function(object, key){
+		var txt = object[key + App.language];
+		if(txt == undefined)
+			return object[key];
+		return txt; 
+	},
+	
+	setLanguageEnglish:function(){
+		App.language = "-en";
+	},
+	
+	setLanguageSpanish:function(){
+		App.language = "";
+	},
+	
+	setLanguageDefault:function(){
+		App.setLanguageSpanish();
+	},
+	
+	isLanguageEnglish:function(){
+		return (App.language == "-en");
+	},
+	
+	isLanguageSpanish:function(){
+		return (App.language == "" || App.language == "-es");
+	},
+	
 	getNextAdScreen:function(loadSuccess, loadError){
 		ModelManager.getAll(ModelManager.type.adScreen.code, 
 			function(result){
